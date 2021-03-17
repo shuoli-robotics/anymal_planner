bc64f7a9d385fe74c9cdf3d8f27b0ffff4a814bd
Shuo Li <ls90911@gmail.com>
2021-03-17 19:28:46 +0000

    aa

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-    packages=['test_pkg'],
+    packages=['anymal_planning','test_pkg'],
     package_dir={'': 'src'},
 )
 
